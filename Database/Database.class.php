6416a6418fb41150ea46338fc3ad409d34044e19
MattBaker99 <19010619@hope.ac.uk>
2022-04-07 15:29:57 +0100

    Database Class, some of users class

---
+++ Database/Database.class.php
@@ -0,0 +1,33 @@
+<?php
+  class Database
+  {
+    public static $host = "localhost";
+    public static $username = "root";
+    public static $password = "root";
+    public static $dbname = "connectVLE";
+    
+    protected static function query(string $sql, array $args)
+    {
+      $stmt = self::connect()->prepare($sql);
+
+      if (explode(" ", $sql)[0] == "SELECT")
+      {
+        $stmt->execute($args);
+        return $stmt->fetchAll();
+      }
+      else
+      {
+        return $stmt->execute($args);
+      }
+    }
+    
+    protected static function connect()
+    {
+      $dsn = "mysql:host=" . Database::$host . ";dbname=" . Database::$dbname;
+      $pdo = new PDO($dsn, Database::$username, Database::$password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $pdo;
+    }
+    
+  }
+?>
